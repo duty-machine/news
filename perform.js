a640784ca5b61cbad6e6286c6c5d5ad28ca39bff
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-06-29 03:22:15 -0300

    add rfa

--- perform.js
+++ perform.js
@@ -21,6 +21,8 @@ async function fetchArticles(site) {
     articles = await fetchFeedx(site, feedxUrls[site])
   } else if (site == '中国数字时代') {
     articles = await fetchCDT()
+  } else if (site == '自由亚洲电台') {
+    articles = await fetchRFA()
   }
 
   articles.sort((x, y) => x.pubDate - y.pubDate)
@@ -65,6 +67,21 @@ async function fetchCDT() {
   })
 }
 
+async function fetchRFA() {
+  let parser = new Parser()
+  let feed = await parser.parseURL('https://www.rfa.org/mandarin/yataibaodao/rss2.xml')
+
+  return feed.items.map(item => {
+    return {
+      title: item.title,
+      content: item['content:encoded'],
+      link: item.guid,
+      pubDate: Date.parse(item.pubDate),
+      site: '自由亚洲电台'
+    }
+  })
+}
+
 async function perform() {
   let sites = Object.keys(feedxUrls)
 
@@ -72,6 +89,7 @@ async function perform() {
     performSite(site)
   })
   performSite('中国数字时代')
+  performSite('自由亚洲电台')
 }
 
 async function performSite(site) {
